26c6f0bf6567f3da96676b5c7bc102145cce7d96
USFrozen <acefrozen1@gmail.com>
2025-06-16 12:51:25 -0500

    CH2 L5: num_words is now in stats.py and imported into main

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
+from stats import num_words
+
 def get_book_text(f):
     with open(f) as book:
         return book.read()
-
-def num_words(book):
-    words = book.split()
-    return len(words)
 
 def main():
     file = "/home/frozen/workspace/github.com/USFrozen/bookbot/books/frankenstein.txt"
