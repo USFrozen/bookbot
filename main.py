b31b5c9abf961abe8a96d8d90de362e24cfe95f4
USFrozen <acefrozen1@gmail.com>
2025-06-16 12:42:44 -0500

    CH2 L4: main.py now returns word count

--- main.py
+++ main.py
@@ -2,9 +2,12 @@ def get_book_text(f):
     with open(f) as book:
         return book.read()
 
+def num_words(book):
+    words = book.split()
+    return len(words)
+
 def main():
     file = "/home/frozen/workspace/github.com/USFrozen/bookbot/books/frankenstein.txt"
-    get_book_text(file)
-    print(get_book_text(file))
+    print(f"{num_words(get_book_text(file))} words found in the document")
 
 main()
